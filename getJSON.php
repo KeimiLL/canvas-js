bedb9a0c2eb9b76f6332ba72d94e1c0521dcfc16
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2022-12-01 07:48:38 +0100

    Added very first version of getJSON.php

--- getJSON.php
+++ getJSON.php
@@ -9,15 +9,7 @@
 // echo '<a href="#"><img style="background-color:white;" border="3px solid black" width="30%" src="'.$json_decoded->img.'"></a>';
 
 
-$jsonData = file_get_contents("canvas_data.json");
-$json_decoded = (array)json_decode($jsonData);
+$jsonData = file_get_contents("json_data/data.json");
+$json_decoded = json_decode($jsonData);
 
-foreach ($json_decoded as $obj) {
-    echo '<a href="#"><img style="background-color:white;" border="3px solid black" width="100px" src="';
-    echo $obj;
-    echo '"></a><br><br>';
-
-    // echo '<p color="white">';
-    // echo $obj;
-    // echo '</p>';
-}
+echo $json_decoded;
